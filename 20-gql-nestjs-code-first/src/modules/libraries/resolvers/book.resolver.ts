245bd2e5aa8941df5b797bd26e0b1195416f18a1
Anton Gulyaev <kishmyak2001@gmail.com>
2021-05-22 16:43:09 +0300

    Added changeBookName method and deleted addBook method

--- 20-gql-nestjs-code-first/src/modules/libraries/resolvers/book.resolver.ts
+++ 20-gql-nestjs-code-first/src/modules/libraries/resolvers/book.resolver.ts
@@ -1,40 +1,44 @@
-import { Args, Parent, Query, ResolveField, Resolver, Mutation } from '@nestjs/graphql';
-import { Book } from '../gql-types/book.gql-type';
-import { Author } from '../gql-types/author.gql-type';
-import { AuthorModelDataModel, authors, BookModelDataModel } from '../../../mocks/data.mocks';
-import { books } from '../../../mocks/data.mocks';
-import { BookGqlMutationArgsType } from '../gql-types/book.gql-mutation-args.type';
+import {Args, Parent, Query, ResolveField, Resolver, Mutation} from '@nestjs/graphql';
+import {Book} from '../gql-types/book.gql-type';
+import {Author} from '../gql-types/author.gql-type';
+import {AuthorModelDataModel, authors, BookModelDataModel} from '../../../mocks/data.mocks';
+import {books} from '../../../mocks/data.mocks';
+import {BookGqlMutationArgsType} from '../gql-types/book.gql-mutation-args.type';
 
 @Resolver(() => Book)
 export class BookResolver {
 
-  @Query(() => [Book])
-  books(
-    @Args('id', { nullable: true }) bookId: string
-  ): BookModelDataModel[] {
-    if (bookId) {
-      return [books.find(book => book.id === bookId)];
+    @Query(() => [Book])
+    books(
+        @Args('id', {nullable: true}) bookId: string
+    ): BookModelDataModel[] {
+        if (bookId) {
+            return [books.find(book => book.id === bookId)];
+        }
+        return books;
     }
-    return books;
-  }
 
-  @Mutation(type => Book)
-  addBook(@Args() mutationArgs: BookGqlMutationArgsType) {
-    const lastBook = books[books.length - 1]
-    const newId = lastBook.id + 1
-    const newBook: BookModelDataModel = {
-      id: newId,
-      ...mutationArgs
-    };
-    books.push(newBook);
-    return newBook;
-  }
+    @Mutation(type => Book)
+    changeBookName(@Args() mutationArgs: BookGqlMutationArgsType) {
+        const bookToChange = books.find(book => book.id === mutationArgs.id
+            && book.title != mutationArgs.title
+            && book.author_id === mutationArgs.author_id
+            && book.publicationDate === mutationArgs.publicationDate);
 
-  @ResolveField(() => Author)
-  author(
-    @Parent() book: BookModelDataModel
-  ): AuthorModelDataModel {
-    return authors.find(author => book.author_id === author.id);
-  }
+        switch (bookToChange) {
+            case null:
+                return null
+            default:
+                bookToChange.title = mutationArgs.title
+                return bookToChange
+        }
+    }
+
+    @ResolveField(() => Author)
+    author(
+        @Parent() book: BookModelDataModel
+    ): AuthorModelDataModel {
+        return authors.find(author => book.author_id === author.id);
+    }
 
 }
